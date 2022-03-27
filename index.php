140ab21395ffeff90af81c1b090c2a72aaeb812e
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-27 15:52:18 -0300

    blog_php_udemy

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- TEMPLATES -->
+<?php
+    include_once("helpers/url.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css" type="text/css">
+
+</head>
+
+<body>
+    <h1>ola mundo</h1>
+
+</body>
+
+</html>
